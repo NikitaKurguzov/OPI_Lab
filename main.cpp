73a85b7daac92aaca816c8d2b3e56772c993ce5c
NikitaKurguzov <2020100197@pnu.edu.ru>
2022-02-15 15:39:26 +1000

    added more comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-
+//this code does these things:
+//it runs
+//it exists
 #include <iostream>
 #include "Functions.h"
 
 int main()
 {
+	//array that we will work with
 	int* mass = new int[7]{ 1,2,3,5,-85,0,89 };
+	//index
 	int ind = -1;
 	int res = FindSmallest(mass, 7, ind);
+	//output
 	std::cout << "Min: " << res << " Index: " <<ind <<std::endl;
 }
