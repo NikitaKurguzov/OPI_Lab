95f0e697421261ea5c327d062d4aca3d69409519
NikitaKurguzov <2020100197@pnu.edu.ru>
2022-02-18 15:04:47 +1000

    added docs

---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,18 @@
+var NAVTREEINDEX0 =
+{
+"_functions_8cpp.html":[0,0,0],
+"_functions_8cpp.html#a2c5e2494e16a2c329979f7b1254c092b":[0,0,0,0],
+"_functions_8h.html":[0,0,1],
+"_functions_8h.html#a2c5e2494e16a2c329979f7b1254c092b":[0,0,1,2],
+"_functions_8h.html#a7e6d63fd0a8d4e103e1ddd8be8b29096":[0,0,1,1],
+"_functions_8h.html#a8a6d8b43381b688b61365ed1713561be":[0,0,1,0],
+"_functions_8h.html#a915f41aca404e22e569ecea889e1e035":[0,0,1,3],
+"_functions_8h_source.html":[0,0,1],
+"files.html":[0,0],
+"globals.html":[0,1,0],
+"globals_func.html":[0,1,1],
+"index.html":[],
+"main_8cpp.html":[0,0,2],
+"main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4":[0,0,2,0],
+"pages.html":[]
+};
